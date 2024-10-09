038efa886a06729af52c011b95f1a1b4c0eb1a5e
John Lyle <johnlyleiv@gmail.com>
2024-10-09 11:09:19 -0500

    fixed logging interval for faster runs

--- rl_demo/callbacks.py
+++ rl_demo/callbacks.py
@@ -2,11 +2,15 @@ import wandb
 from stable_baselines3.common.callbacks import BaseCallback
 
 
+
 class WandBVideoCallback(BaseCallback):
     def __init__(self, verbose=0) -> None:
         super(WandBVideoCallback, self).__init__(verbose)
+        self.counter = 0
 
     def _on_step(self) -> bool:
-        if self.training_env.plot is not None:
+        if self.training_env.plot is not None and self.counter > 1000:
             wandb.log({"plot": wandb.Image(self.training_env.plot)})
+            self.counter = 0    
+        self.counter += 1
         return True
