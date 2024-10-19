908813c0122e02c2ff26ca0ff1766b3454dbc751
Will <william.lipscomb@austin.utexas.edu>
2024-10-19 12:39:25 -0500

    Added effort_penalty and altered the goal tracking and goal reached rewards.

--- rl_demo/rewards.py
+++ rl_demo/rewards.py
@@ -10,13 +10,13 @@ def double_integrator_rewards(x: np.ndarray, u: np.ndarray) -> np.ndarray:
     """
     # task rewards
     pose_error = np.linalg.norm(x[:, 6:8] - x[:, 0:2], axis=1)
-    tracking_std = 10
-    goal_tracking = np.exp(-pose_error ** 2 / tracking_std)
-    goal_reached = np.where(pose_error < 0.1, 1, 0)
+    tracking_std = 3000
+    goal_tracking = 10*np.exp(-pose_error**2/tracking_std)
+    goal_reached = np.where(pose_error < 1, 1, 0)
+    #wall_hit = np.where((x[:, 0] == 300) | (x[:,0] == -300), 1, 0)
 
     # regularizing rewards
     effort_penalty = -np.linalg.norm(u, axis=1) ** 2
-    action_rate = -np.linalg.norm(u - x[:, 4:6], axis=1)
+    # action_rate = -np.linalg.norm(u - x[:, 4:6], axis=1)
 
-    return 5 * goal_tracking + 5 * goal_reached + \
-        0.0001 * effort_penalty + 0.0001 * action_rate
+    return 10 * goal_reached + goal_tracking + .5 * effort_penalty
